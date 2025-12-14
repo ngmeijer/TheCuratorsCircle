6306ee1e6f2bd7bf257b5f7e39cd63435db363ea
Nils Meijer <meijernils9@gmail.com>
2025-12-14 16:49:45 +0100

    VECT-1 implemented signup endpoint

--- Frontend/TheCuratorsCircle/api/auth/auth.ts
+++ Frontend/TheCuratorsCircle/api/auth/auth.ts
@@ -24,3 +24,30 @@
 
     return response.json();
 }
+
+export async function signup(email : string, password: string) {
+    const json = JSON.stringify({email, password});
+
+    const response = await fetch("http://100.90.173.113:5044/auth/signup", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: json,
+    });
+
+    let data;
+
+    if(!response.ok) {
+        try {
+            data = await response.json();
+        } catch {
+            // Fallback if body is empty or not JSON
+            data = { message: "Unknown error."};
+        }
+
+        throw new Error(data.message);
+    }
+
+    return response.json();
+}
